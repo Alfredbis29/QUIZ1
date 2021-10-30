42b3f81880cd0132f300e27dddda7c19dea1afe6
Alfredbis29 <kalumunabisimwa5@gmail.com>
2021-10-30 19:14:34 +0200

    Changed conditions

--- script.js
+++ script.js
@@ -135,7 +135,7 @@ answers: [
   answers: [
     { text: '18 ', correct: false },
     { text: '67', correct: false },
-    { text: '21', correct: true },
+    { text: '22', correct: true },
     { text: '45', correct: false }
   ]
 },
