dea9c4e506e0775e9e56ce2d8f908f37d12a43cd
Alfredbis29 <kalumunabisimwa5@gmail.com>
2021-10-31 20:36:38 +0200

    Changed conditions

--- script.js
+++ script.js
@@ -115,7 +115,7 @@ const questions = [
 question: 'What is  my food fav?',
 answers: [
   { text: 'bread', correct: false },
-  { text: 'fish', correct: false },
+  { text: 'fish', correct: true },
   { text: 'steak', correct: false },
   { text: 'sandwich', correct: true }
 ]
