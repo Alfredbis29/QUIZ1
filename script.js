fbbfb6dbf71753f9ab7ce0b8278a88afc5022c42
Alfredbis29 <kalumunabisimwa5@gmail.com>
2021-11-17 13:38:00 +0200

    Add quiz

--- script.js
+++ script.js
@@ -78,7 +78,7 @@ function clearStatusClass(element) {
 
 const questions = [
   {
-    question: 'What is 10 + 3?',
+    question: 'What is 10 + 4?',
     answers: [
       { text: '13', correct: true },
       { text: '22', correct: false }
