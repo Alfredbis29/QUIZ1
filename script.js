a0774cd244354e0c0dc6595542f14a0ad1771f78
Alfredbis29 <kalumunabisimwa5@gmail.com>
2021-12-13 17:56:19 +0200

    Changed condition

--- script.js
+++ script.js
@@ -87,7 +87,7 @@ const questions = [
   {
     question: 'Who is Alfred Bisimwa?',
     answers: [
-      { text: 'Web dev ', correct: true },
+      { text: 'full-stuck dev ', correct: true },
       { text: 'Musician', correct: false },
       { text: 'Dev Ed', correct: false },
       { text: 'Fun Fun Function', correct: false }
